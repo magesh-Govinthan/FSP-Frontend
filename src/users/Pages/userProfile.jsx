50b6c28c7d8e2a28b5c41dab263b21137be95c32
magesh-Govinthan <mageshgovinthan16@gmail.com>
2026-04-09 19:13:58 +0530

    add

--- src/users/Pages/userProfile.jsx
+++ src/users/Pages/userProfile.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Container, Card, Button, Form, Alert } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import "./UserProfile.css";
 import axios from "axios";
 
 const UserProfile = () => {
