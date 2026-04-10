cba9f4d348dea1cecc4b256b0e488ebb81c52b7b
magesh-Govinthan <mageshgovinthan16@gmail.com>
2026-04-10 19:01:31 +0530

    add

--- src/users/Pages/Failed.jsx
+++ src/users/Pages/Failed.jsx
@@ -2,19 +2,23 @@ import { useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import './Success.css'
+
 export default function PaymentFailed() {
-    const navigate = useNavigate();
-    useEffect(() => {
-        setTimeout(() => {
-            navigate("/");
-        }, 3000);
-    }, []);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/");
+    }, 3000);
+  }, []);
+
   return (
-    <Container className="text-center py-5">
-      <Row className="justify-content-center">
-        <Col xs="auto">
+    <Container className="text-center py-5 d-flex flex-column justify-content-center align-items-center min-vh-100">
+      
+      <Row className="justify-content-center w-100">
+        <Col xs={12} sm={8} md={6} lg={4} className="d-flex justify-content-center">
           <svg
-            className="fail-cross"
+            className="fail-cross img-fluid"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 52 52"
             width="100"
@@ -26,12 +30,18 @@ export default function PaymentFailed() {
           </svg>
         </Col>
       </Row>
-      <Row>
-        <Col>
-          <h2 className="text-danger mt-3">Payment Failed</h2>
-          <p>Something went wrong. Please try again.</p>
+
+      <Row className="w-100">
+        <Col xs={12}>
+          <h2 className="text-danger mt-3 fs-4 fs-md-3 fs-lg-2">
+            Payment Failed
+          </h2>
+          <p className="px-3 px-md-0">
+            Something went wrong. Please try again.
+          </p>
         </Col>
       </Row>
+
     </Container>
   );
 }
